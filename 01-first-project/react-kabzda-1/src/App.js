441ed9f6fd99dc52f67b14952cdc616702c13bda
Fundorin174 <fundorin174@gmail.com>
2019-09-30 09:45:21 -0700

    start

---
+++ 01-first-project/react-kabzda-1/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <div>One div</div>
+      <div className="App">
+      <ul>
+        <li>Список</li>
+        <li>css</li>
+        <li>html</li>
+        <li>js</li>
+        <li>react</li>
+      </ul>
+    </div> 
+    </div>
+  );
+}
+
+export default App;
