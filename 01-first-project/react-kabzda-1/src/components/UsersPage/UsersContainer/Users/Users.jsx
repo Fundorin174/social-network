430cb93ae96b3f40761207ccc4ab57e5fc8fcb69
Fundorin174 <fundorin174@gmail.com>
2019-12-16 20:17:18 +0300

    55lessonhalf151219

--- 01-first-project/react-kabzda-1/src/components/UsersPage/UsersContainer/Users/Users.jsx
+++ 01-first-project/react-kabzda-1/src/components/UsersPage/UsersContainer/Users/Users.jsx
@@ -12,32 +12,38 @@ class Users extends React.Component {
             .then(response => {
                 this
                     .props
-                    .setUsers(response.data.items)
+                    .setUsers(response.data.items);
             });
     }
 
     render() {
-        return <div>
-            <div className={classes.search_wrapper}>
-                <Search searchUsers={this.props.searchUsers}/>
+        return (
+            <div>
+                <div className={classes.search_wrapper}>
+                    <Search searchUsers={this.props.searchUsers}/>
+                </div>
+                <div className={classes.users_wrapper}>
+                    {
+                        this
+                            .props
+                            .users
+                            .map(user => {
+                                if (`${user.name}`.toLowerCase().includes(this.props.searchUsersText.toLowerCase())) {
+                                    return <User key={user.id} id={user.id} fullName={user.name} follow={user.followed}
+                                                 smallPhotoSrc={user.photos.small}
+                                        // country={user.residency.country}
+
+                                        // city={user.residency.city}
+                                                 status={user.status} isFrend={user.isFrend} toFollow={this.props.toFollow}
+                                                 toUnfollow={this.props.toUnfollow} toFriends={this.props.toFriends}
+                                                 fromFriends={this.props.fromFriends}/>
+                                }
+                            })
+                    }
+                </div>
             </div>
-            <div className={classes.users_wrapper}>
-                {
-                    this
-                        .props
-                        .users
-                        .map(user => {
-                            if (`${user.name}`.toLowerCase().includes(this.props.searchUsersText.toLowerCase())) {
-                                return <User key={user.id} id={user.id} fullName={user.name} follow={user.followed} smallPhotoSrc={user.photos.small}
-                                    // country={user.residency.country}
-                                    
-                                    // city={user.residency.city}
-                                    status={user.status} isFrend={user.isFrend} toFollow={this.props.toFollow} toUnfollow={this.props.toUnfollow} toFriends={this.props.toFriends} fromFriends={this.props.fromFriends}/>
-                            }
-                        })
-                }
-            </div>
-        </div>
+            )
+
     }
 
 }
