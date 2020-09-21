d6a2ef63892b1944cf7a23353d06daf74472bb87
Fundorin174 <Fundorin174@gmail.com>
2020-09-21 20:34:54 +0300

    deploy

--- 01-first-project/react-kabzda-1/build/service-worker.js
+++ 01-first-project/react-kabzda-1/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/social-network/precache-manifest.beda357b3344366e91da7436e87fdee8.js"
+  "/social-network/precache-manifest.e6eb43bd4129a9bcec49145cb0f8dea7.js"
 );
 
 self.addEventListener('message', (event) => {
